ff953fda469b0cfb12c9d9b690353f4f28e6a800
Bashriov <Agil@CADR2.code.local>
2021-01-20 14:20:17 +0400

    client page added

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Navbar elements
 const navToggle = document.querySelector(".nav-toggle");
 const links = document.querySelector(".links");
 const socialIcons = document.querySelector(".nav-icons");
@@ -12,3 +13,62 @@ navToggle.addEventListener("click", () => {
 
 window.addEventListener("resize", () => links.classList.remove("show-links"));
 window.addEventListener("resize", () => socialIcons.classList.remove("show-nav-icons"));
+
+
+// Slider elements
+const prevBtn = document.querySelector('.prev-btn');
+const nextBtn = document.querySelector('.next-btn');
+imageBox = document.querySelector('.image-box');
+TextBox = document.querySelector('.text-content');
+
+
+prevBtn.addEventListener('click', (e)=>{
+	e.preventDefault();
+	slideToPrev();
+	clearInterval(autoRotate,4000);
+});
+
+nextBtn.addEventListener('click', (e)=>{
+	e.preventDefault();
+	slideToNext();
+	clearInterval(autoRotate,4000);
+
+});
+
+
+// Slider functions
+
+function slideToPrev(){
+	const activeImage = document.querySelector('.image.active');
+	const activeText = document.querySelector('.text.active');
+	activeImage.classList.remove('active');
+	activeText.classList.remove('active');
+
+	if(activeImage.previousElementSibling != null){
+		activeImage.previousElementSibling.classList.add('active');
+		activeText.previousElementSibling.classList.add('active');
+	}
+	else{
+		imageBox.lastElementChild.classList.add('active');
+		TextBox.lastElementChild.classList.add('active');
+	}
+
+}
+
+function slideToNext(){
+	const activeImage = document.querySelector('.image.active');
+	const activeText = document.querySelector('.text.active');
+	activeImage.classList.remove('active');
+	activeText.classList.remove('active');
+
+	if(activeImage.nextElementSibling != null){
+		activeImage.nextElementSibling.classList.add('active');
+		activeText.nextElementSibling.classList.add('active');
+	}
+	else{
+		imageBox.firstElementChild.classList.add('active');
+		TextBox.firstElementChild.classList.add('active');
+	}
+}
+
+const autoRotate = setInterval(slideToNext, 4000);
